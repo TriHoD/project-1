2e619ce340622c9506b23884aa45a0039d8d175d
Ho Dac Tri <miugypig@Hos-MacBook-Pro.local>
2019-10-07 14:18:32 -0700

    mouse cursor and end game

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+$(document).mousemove(function (e) { 
+    // $('#follower').css({left: e.clientX, top: e.clientY});
+    setTimeout(function() {
+        $('#follower').css({left: e.clientX, top: e.clientY});
+    }, 1);
+});
+
 class Game {
     constructor(name) {
         this.name = name
@@ -6,8 +13,9 @@ class Game {
 
         this.timeLine = new TimelineMax({smoothChildTiming: true}) // 
         this.speed = 1.5 // the initial drop speed of the ball
-        this.colors = []; // the colors will be using in the game
-        this.colorRGBA = [];
+        this.colors = ['#bf0a0a', '#13803b', '#37b5f0']; // the colors will be using in the game
+        this.colorRGB = ['rgb(191, 10, 10)', 'rgb(19, 128, 59)', 'rgb(55, 181, 240)'];
+        this.color = this.color[0] // the initial color of the gem
         
 
 
@@ -18,9 +26,16 @@ class Game {
         this.scale = (this.screen > this.screenHeight) ? this.screenHeight/800 : this.screen/1200;
     }
 
-    generateObject() {
-        
+    generateGems() {
+        this.gemBetween = new TimelineMax({repeat: -1, paused: 1});
+        $('.scene .ball-holder').append('<div class="ball red" id="ball"></div>');
+        this.bounce();
     }
 
+    generateTweet() {
+        let top = $(window).height() / 2 - 150;
+        let left = $(window).width() / 2 - 300;
+        window.open("https://twitter.com/intent/tweet?url=https://codepen.io/gregh/full/yVLOyO&amp;text=I scored "+ this.score +" points on Coloron! Can you beat my score?&amp;via=greghvns&amp;hashtags=coloron", "TweetWindow", "width=600px,height=300px,top=" + top + ",left=" + left);
+    }
 }
 
